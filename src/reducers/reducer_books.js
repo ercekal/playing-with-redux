7a7d4d5f0061386653e4b093ef3399afa431279f
ercekal <ercekalabalikoglu@gmail.com>
2016-12-09 18:05:47 +0000

    book selector app with reduc is basically done;

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JS: The good', pages:100 },
+    { title: 'Harry Potter', pages:101 },
+    { title: 'Papillon', pages:102 },
+    { title: 'Ruby', pages:103 }
+  ]
+}
